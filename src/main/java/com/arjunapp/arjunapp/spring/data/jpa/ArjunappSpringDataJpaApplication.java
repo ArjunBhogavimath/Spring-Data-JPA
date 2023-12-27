1901f001864253c07bda4ddbbb97df412b4d535f
Mallikarjunaiah B M <vpmallikarjuna03@gmail.com>
2023-12-27 21:11:15 +0530

    Initial commit

---
+++ src/main/java/com/arjunapp/arjunapp/spring/data/jpa/ArjunappSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.arjunapp.arjunapp.spring.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjunappSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjunappSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arjunapp/arjunapp/spring/data/jpa/ArjunappSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjunapp.arjunapp.spring.data.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjunappSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
